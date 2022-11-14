2fd9dba429dd1283375972f7357c1dd0f60acf01
Christopher Torres Aceituno <christopher.torres@usach.cl>
2022-11-14 00:56:45 -0300

    Frontend full view working

--- voluntariado-fe-TBD/pages/minor_ranking.vue
+++ voluntariado-fe-TBD/pages/minor_ranking.vue
@@ -26,18 +26,21 @@
                             </b-col>
                         </b-row>
                     </b-container>
-                    <b-container class="fluid">
+                    <b-container>
                         <b-row>
-                            <button class="btn btn-primary" @click="getEmergencias">Buscar emergencias en el periodo ingresado</button>
+                            <button class="btn btn-primary" @click="getEmergencias">Buscar emergencias en el periodo
+                                ingresado</button>
                         </b-row>
                     </b-container>
                     <b-container>
                         <b-row>
                             <b-col>
-                                <b-table bordered hover :items="emergencias" :fields="fields1" dark @row-clicked="getTareasByEmergencia" ></b-table>
+                                <b-table bordered hover :items="emergencias" :fields="fields1" dark
+                                    @row-clicked="getTareasByEmergencia"></b-table>
                             </b-col>
                             <b-col>
-                                <b-table bordered hover :items="tareas" dark @row-clicked="getTareasByEmergencia()"></b-table>
+                                <b-table bordered hover :items="tareas" :fields="fields2" dark>
+                                </b-table>
                             </b-col>
                         </b-row>
                     </b-container>
@@ -53,9 +56,10 @@ export default {
     data() {
         return {
             inicio: '',
-            fin: '', 
-            fields1: ['id', 'nombre'],
+            fin: '',
+            fields1: [{ key: 'id', label: 'Id' }, { key: 'nombre', label: 'Emergencia' }],
             tareas: [],
+            fields2: [{ key: 'id', label: 'Id' }, { key: 'nombre', label: 'Tarea' }, { key: 'cant_vol_inscritos', label: 'Voluntarios inscritos' }, { key: 'descrip', label: 'Menor ranking' }],
             emergencias: [],
             id_emergencia: ''
         }
@@ -70,9 +74,20 @@ export default {
             }).then(response => {
                 this.emergencias = response.data;
             })
-            .catch(error => {
-                console.log(error);
-            });
+                .catch(error => {
+                    console.log(error);
+                });
+        },
+        getTasksByIdVolunteer(id_tarea, i) {
+            let ranking = '';
+            this.$axios.get("http://localhost:8081/voluntario/min/" + id_tarea)
+                .then(response => {
+                    ranking = response.data[0].nombre;
+                    this.tareas[i].descrip = ranking;
+                })
+                .catch(error => {
+                    console.log(error);
+                });
         },
         getTareasByEmergencia(object) {
             let id = object.id;
@@ -81,12 +96,19 @@ export default {
                     emergencia: id
                 }
             }).then(response => {
-                
                 this.tareas = response.data;
+                for (let i = 0; i < this.tareas.length; i++) {
+                    this.tareas[i].descrip = '';
+                }
+
+                for (let i = 0; i < this.tareas.length; i++) {
+                    this.getTasksByIdVolunteer(this.tareas[i].id, i);
+                }
+                console.log(this.tareas);
             })
-            .catch(error => {
-                console.log(error);
-            });
+                .catch(error => {
+                    console.log(error);
+                });
         },
         print() {
             console.log(this.inicio)
